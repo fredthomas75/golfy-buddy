fc1f75cd0606f665c53ce3b2a26ea081f3188c35
Frederic Thomas <fred@mt2lab.com>
2019-08-27 16:19:12 -0400

    Migration gender + update users + new seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_184352) do
+ActiveRecord::Schema.define(version: 2019_08_27_200647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,7 +151,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_184352) do
     t.string "last_name"
     t.string "phone"
     t.date "birth_date"
-    t.integer "gender"
+    t.string "gender"
     t.string "language"
     t.string "current_city"
     t.float "handicap"
