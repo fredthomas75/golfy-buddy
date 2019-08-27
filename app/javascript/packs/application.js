3625d863c3f0af61640aad7dc8df12599d330b19
Louis-Olivier Pelletier <louisolivierpelletier@gmail.com>
2019-08-27 17:43:50 -0400

    Sign up avec tous les champs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,5 +5,5 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 initMapbox();
-import { placesAutocomplete } from '../plugins/autocomplete';
-placesAutocomplete();
+import { initUserAutocomplete } from '../plugins/autocomplete';
+initUserAutocomplete();
--- app/javascript/plugins/autocomplete.js
+++ app/javascript/plugins/autocomplete.js
@@ -1,12 +1,10 @@
-console.log('Salut from icitte!')
+import places from 'places.js';
 
-var places = require('places.js');
-var placesAutocomplete = places({
-  appId: 'plZ3DRI7CQ0K',
-  // appId: 'YOUR_PLACES_APP_ID',
-  apiKey: '9c822ff7212e6fed7e4def6d5279482b',
-  // apiKey: 'YOUR_PLACES_API_KEY',
-  container: document.querySelector('#current_city')
-});
+const initUserAutocomplete = () => {
+  const addressInput = document.getElementById('current_city');
+  if (addressInput) {
+    places({ container: addressInput });
+  }
+};
 
-export { placesAutocomplete }
+export { initUserAutocomplete };
