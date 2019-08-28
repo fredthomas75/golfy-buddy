e0475b747422f2cf8947258a62377f4273f80a80
Younes <mia.san.younes@gmail.com>
2019-08-28 11:01:25 -0400

    User can join a game and join button disapears if he has already joined/game is full

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,5 @@ class Game < ApplicationRecord
   has_many :guests, dependent: :destroy
   has_many :attachments, as: :attachable, dependent: :destroy
   accepts_nested_attributes_for :attachments
+  validates :number_players, presence: true
 end
