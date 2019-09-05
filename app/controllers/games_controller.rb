a2787d22ba14e0d68b90f94f96bf76b141b1f9bf
Frederic Thomas <fred@mt2lab.com>
2019-09-05 13:07:20 -0400

    css for paginate

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -30,6 +30,7 @@ class GamesController < ApplicationController
       @past_games = @past_games.global_search(params[:query])
     else
       @games = Game.all
+      @games = @games.paginate(page: params[:page], per_page: 12)
       @guests = Guest.where(game: [@games])
     end
   end
